d7fbc551d1420211c75f02694990762853d71da6
Natan Felles <natanfelles@gmail.com>
2021-06-17 18:19:20 -0300

    Initial commit for Functions Feature

---
+++ src/Functions.php
@@ -0,0 +1,61 @@
+<?php namespace Framework\Database;
+
+use Closure;
+
+/**
+ * Class Functions.
+ *
+ * @see https://mariadb.com/kb/en/built-in-functions/
+ */
+class Functions
+{
+	protected Database $database;
+
+	public function __construct(Database $database)
+	{
+		$this->database = $database;
+	}
+
+	protected function renderValue(
+		Closure | int | string | float | null $value
+	) : string | float | int {
+		return $value instanceof Closure
+			? '(' . $value($this->database) . ')'
+			: $this->database->quote($value);
+	}
+
+	public function same(mixed $value) : Closure
+	{
+		return static function () use ($value) : mixed {
+			return $value;
+		};
+	}
+
+	public function concat(string $value1, string $value2, string ...$values) : Closure
+	{
+		$values = [$value1, $value2, ...$values];
+		foreach ($values as &$value) {
+			$value = $this->renderValue($value);
+		}
+		unset($value);
+		$values = \implode(', ', $values);
+		return static function () use ($values) : string {
+			return 'CONCAT(' . $values . ')';
+		};
+	}
+
+	public function hex(Closure | int | string | null $value) : Closure
+	{
+		$value = $this->renderValue($value);
+		return static function () use ($value) : string {
+			return 'HEX(' . $value . ')';
+		};
+	}
+
+	public function now() : Closure
+	{
+		return static function () : string {
+			return 'NOW()';
+		};
+	}
+}
